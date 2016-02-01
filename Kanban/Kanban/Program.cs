1d669a207251a212bb1928e7a3add23787beff63
Michael Murray <Michael Murray>
2016-02-01 11:58:49 -0800

    Initial Commit

---
+++ Kanban/Kanban/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanban
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new KanbanEntities())
+            {
+                foreach (var list in db.Lists)
+                {
+                    Console.WriteLine(list.Name);
+                    foreach (var card in db.Cards)
+                    {
+                        if (card.ListID == list.ListID)
+                        {
+                            Console.WriteLine("\t" + card.Text);
+                        }
+                    }
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
